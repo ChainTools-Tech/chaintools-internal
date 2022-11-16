77ff8842f8863e5629f0d22b056d60f3c5b13b46
Highlander-Maker <highlander1952@protonmail.com>
2022-11-16 12:08:16 +0000

    add voting quick commands

---
+++ quick_commands/voting.sh
@@ -0,0 +1,19 @@
+
+
+kujirad tx gov vote 77 abstain --from Highlander-Main --gas auto --gas-adjustment 1.2 --fees 2500ukuji --chain-id kaiyo-1 --node https://rpc.kujira.chaintools.tech:443
+
+chihuahuad tx gov vote 41 yes --from Highlander-Nodes --fees 5000uhuahua --chain-id chihuahua-1 --node https://rpc.chihuahua.wtf:443
+
+
+comdex tx gov vote 9 yes --from Highlander-Nodes --gas auto --gas-adjustment 1.2 --fees 2500ucmdx --chain-id comdex-1 --node https://rpc.comdex.one:443
+
+
+junod tx gov vote 51 yes --from Highlander-Nodes --gas-prices 0.025ujuno --chain-id juno-1 --node https://rpc-juno.whispernode.com:443
+
+
+gaiad tx gov vote 79 yes --from Chaintools --chain-id cosmoshub-4 --fees 1500uatom --node https://rpc-cosmoshub.whispernode.com:443
+
+
+evmosd tx gov vote 76 yes  --chain-id evmos_9001-2 --gas-prices "50000000000aevmos"  --gas 500000  --node https://evmos-rpc.polkachu.com:443  --from Highlander-Nodes
+
+
